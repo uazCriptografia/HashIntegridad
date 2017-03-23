fef3b38ebe719e661afe7f064225727700ff820e
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-03-22 18:24:26 -0600

    Creación de archivo de prueba

--- src/com/porfirioads/criptografia/hashintegridad/Test.java
+++ src/com/porfirioads/criptografia/hashintegridad/Test.java
@@ -4,4 +4,7 @@ package com.porfirioads.criptografia.hashintegridad;
  * Created by porfirio on 3/22/17.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
 }
